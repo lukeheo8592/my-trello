dd856fb26ff3b0a8ca5ccf566642fbcbb853431a
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2022-01-12 12:00:06 -0500

    make it better not to rerender uselessly with react memo

---
+++ src/components/DragabbleCard.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import {Draggable } from "react-beautiful-dnd";
+import React from "react";
+
+const Card = styled.div`
+border-radius: 5px;
+margin-bottom: 5px;
+padding: 10px 10px;
+background-color: ${(props) => props.theme.cardColor};
+`;
+
+interface IDragabbleCardProps{
+    toDo: string;
+    index: number;
+}
+
+function DragabbleCard({toDo,index}:IDragabbleCardProps){
+    console.log(toDo, "has been rendered");
+    return(<Draggable key={toDo} draggableId={toDo} index={index}>
+        {(magic) => (
+          <Card
+            ref={magic.innerRef}
+            {...magic.dragHandleProps}
+            {...magic.draggableProps}
+          >
+            {toDo}
+          </Card>
+        )}
+      </Draggable>)
+}
+
+export default React.memo(DragabbleCard);
